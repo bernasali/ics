68e748dc0d015eeba2ff3da0ec96bca0dc2f63d3
bernasali <65134190+bernasali@users.noreply.github.com>
2023-05-02 22:04:11 +0300

    Initial changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,4 +11,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name",defaultValue="World")String name){
+		return String.format("Hello %s!", name);
+	}
+
 }
+
